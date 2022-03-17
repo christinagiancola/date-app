5396272e2139b897790e430749f056bdac135d59
Christina Giancola <christinamgiancola@gmail.com>
2022-03-17 11:33:09 -0400

    WIP continued setup with Webpack and Babel

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+	mode: 'production',
 	entry: path.resolve(__dirname, './src/index.js'),
 	module: {
 		rules: [
