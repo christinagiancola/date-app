7e582f62ef3ac2779e735b8bcc790a13266ad38b
Christina Giancola <christinamgiancola@gmail.com>
2022-06-21 15:10:10 -0400

    WIP replace form in <DateForm>, implemented conditionally rendered date-picker

--- src/components/DateForm.js
+++ src/components/DateForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
 import {
 	Container,
 	Stack,
@@ -31,7 +32,7 @@ const DateForm = () => {
 	const [cost, setCost] = useState(0);
 	const [isPetFriendly, setIsPetFriendly] = useState(false);
 	const [doesExpire, setDoesExpire] = useState(false);
-	const [expirationDate, setExpirationDate] = useState(null);
+	const [expirationDate, setExpirationDate] = useState(new Date());
 
 	function handleSubmit(e) {
 		e.preventDefault();
@@ -48,14 +49,14 @@ const DateForm = () => {
 					</InputGroup>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<FormLabel htmlFor='details'>Any extra details?</FormLabel>
 					<InputGroup>
 						<Input type='text' id='details' value={details} onChange={({ target }) => setDetails(target.value)}></Input>
 					</InputGroup>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<FormLabel htmlFor='location'>Where?</FormLabel>
 					<InputGroup>
 						<Input
@@ -67,73 +68,93 @@ const DateForm = () => {
 					</InputGroup>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<FormLabel htmlFor='timeOfDay'>What time of day?</FormLabel>
 					<CheckboxGroup id='timeOfDay'>
 						<Stack direction='row'>
-							<Checkbox value='morning' onChange={({ target }) => setTimeOfDay(target.value)}>
+							<Checkbox value='morning' mr='10px' onChange={({ target }) => setTimeOfDay(target.value)}>
 								Morning
 							</Checkbox>
-							<Checkbox value='afternoon' onChange={({ target }) => setTimeOfDay(target.value)}>
+							<Checkbox value='afternoon' mr='10px' onChange={({ target }) => setTimeOfDay(target.value)}>
 								Afternoon
 							</Checkbox>
-							<Checkbox value='evening' onChange={({ target }) => setTimeOfDay(target.value)}>
+							<Checkbox value='evening' mr='10px' onChange={({ target }) => setTimeOfDay(target.value)}>
 								Evening
 							</Checkbox>
 						</Stack>
 					</CheckboxGroup>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<Stack direction='row'>
 						<FormLabel htmlFor='isRepeatable'>Is the date repeatable?</FormLabel>
-						<Switch id='isRepeatable' onChange={() => setIsRepeatable(!{ isRepeatable })} />
+						<Switch id='isRepeatable' onChange={() => setIsRepeatable(!isRepeatable)} />
 					</Stack>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<Stack direction='row'>
 						<FormLabel htmlFor='isOvernight' defaultValue='false'>
 							Pack an overnight bag?
 						</FormLabel>
-						<Switch id='isOvernight' size='md' onChange={() => setIsOvernight(!{ isOvernight })} />
+						<Switch id='isOvernight' size='md' onChange={() => setIsOvernight(!isOvernight)} />
 					</Stack>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<FormLabel htmlFor='dayOfWeek'>What Days of the Week?</FormLabel>
 					<CheckboxGroup id='dayOfWeek'>
 						<Stack direction='row'>
-							<Checkbox value='weekday' onChange={() => setIsOnWeekday(!{ isOnWeekday })}>
+							<Checkbox value='weekday' onChange={() => setIsOnWeekday(!isOnWeekday)}>
 								Weekday
 							</Checkbox>
-							<Checkbox value='weekend' onChange={() => setIsOnWeekend(!{ isOnWeekend })}>
+							<Checkbox value='weekend' onChange={() => setIsOnWeekend(!isOnWeekend)}>
 								Weekend
 							</Checkbox>
 						</Stack>
 					</CheckboxGroup>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<FormLabel htmlFor='cost'>What's the estimated budget?</FormLabel>
 					<Stack direction='row'>
 						<RadioGroup id='cost'>
-							<Radio value='0'>Free</Radio>
-							<Radio value='1'>$</Radio>
-							<Radio value='2'>$$</Radio>
-							<Radio value='3'>$$$</Radio>
+							<Radio value='0' mr='10px'>
+								Free
+							</Radio>
+							<Radio value='1' mr='10px'>
+								$
+							</Radio>
+							<Radio value='2' mr='10px'>
+								$$
+							</Radio>
+							<Radio value='3' mr='10px'>
+								$$$
+							</Radio>
 						</RadioGroup>
 					</Stack>
 				</FormControl>
 
-				<FormControl>
+				<FormControl mt='15px'>
 					<Stack direction='row'>
-						<FormLabel htmlFor='doesExpire'>Does this idea expire anytime?</FormLabel>
-						<Switch id='doesExpire' size='md' onChange={() => setDoesExpire(!{ doesExpire })} />
+						<FormLabel htmlFor='doesExpire'>Does this date idea expire?</FormLabel>
+						<Switch id='doesExpire' size='md' onChange={() => setDoesExpire(!doesExpire)} />
 					</Stack>
 				</FormControl>
 
-				<FormControl>
+				{doesExpire && (
+					<FormControl>
+						<FormLabel htmlFor='expirationDate'>Expiration Date:</FormLabel>
+						<DatePicker
+							id='expirationDate'
+							showPopperArrow={true}
+							selected={expirationDate}
+							onChange={(date) => setExpirationDate(date)}
+						/>
+					</FormControl>
+				)}
+
+				<FormControl mt='15px'>
 					<Button type='submit'>Add Date</Button>
 				</FormControl>
 			</form>
